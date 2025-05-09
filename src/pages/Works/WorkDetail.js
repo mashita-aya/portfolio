651abe307a4a115d1824ba5cd81d5f508102722e
mashita <axyxa0264@gmail.com>
2025-05-09 16:41:25 +0900

    portfolio_5.9: 端末確認修正

--- src/pages/Works/WorkDetail.js
+++ src/pages/Works/WorkDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { workList } from '../../data/workList';
 import IconWindow from '../../images/window_icon.svg';
+import { FadeInSection } from '../../components/FadeInSection';
 
 export const WorkDetail = () => {
   const { id } = useParams();
@@ -14,78 +15,80 @@ export const WorkDetail = () => {
   }
 
   return (
-    <div className="work_detail_section">
-      <div className="work_detail_header">
-        <h2>
-          {work.url ? (
-            <a target="_blank" rel="noreferrer" href={work.url}>
-              {work.title}
-              <div><img src={IconWindow} alt="外部リンク" /></div>
-            </a>
-          ) : (
-            work.title
-          )}        
-        </h2>
-        <p>{work.summary}</p>
-      </div>
-
-      <div className="work_detail_container">
-        <div className="left_contnt">
-          <div className="sp_img">
-            <img src={`${process.env.PUBLIC_URL}/images/${work.imageName01}`} alt={work.title} />
-          </div>
-          <div className="pc_img">
-            <a 
-              className={work.imageNameSc ? '' : 'full_width'}
-              href={`${process.env.PUBLIC_URL}/images/${work.imageNamePc}`}
-            >
-              <img src={`${process.env.PUBLIC_URL}/images/${work.imageNamePc}`} alt={work.title} />
-            </a>
-            {work.imageNameSc && (
-              <a href={`${process.env.PUBLIC_URL}/images/${work.imageNameSc}`}>
-                <img src={`${process.env.PUBLIC_URL}/images/${work.imageNameSc}`} alt={work.title} />
+    <FadeInSection>
+      <div className="work_detail_section">
+        <div className="work_detail_header">
+          <h2>
+            {work.url ? (
+              <a target="_blank" rel="noreferrer" href={work.url}>
+                {work.title}
+                <div><img src={IconWindow} alt="外部リンク" /></div>
               </a>
-            )}
+            ) : (
+              work.title
+            )}        
+          </h2>
+          <p>{work.summary}</p>
+        </div>
+
+        <div className="work_detail_container">
+          <div className="left_contnt">
+            <div className="sp_img">
+              <img src={`${process.env.PUBLIC_URL}/images/${work.imageName01}`} alt={work.title} />
+            </div>
+            <div className="pc_img">
+              <a 
+                className={work.imageNameSc ? '' : 'full_width'}
+                href={`${process.env.PUBLIC_URL}/images/${work.imageNamePc}`}
+              >
+                <img src={`${process.env.PUBLIC_URL}/images/${work.imageNamePc}`} alt={work.title} />
+              </a>
+              {work.imageNameSc && (
+                <a href={`${process.env.PUBLIC_URL}/images/${work.imageNameSc}`}>
+                  <img src={`${process.env.PUBLIC_URL}/images/${work.imageNameSc}`} alt={work.title} />
+                </a>
+              )}
+            </div>
+          </div>
+          <div className="right_contnt">
+            <ul className="basic-text-inner">
+              <li>
+                <div className="catchCopy">{work.catchCopy}</div>
+                <p className="purpose">{work.summary}</p>
+                {work.detail.split('\n').map((line, index) => (
+                  <React.Fragment key={index}>
+                    <p className="works_introduction">
+                    {line}
+                    </p>
+                  </React.Fragment>
+                ))}
+                <div className="detail">
+                  <dl>
+                    <dt>担当範囲</dt>
+                    <dd>{work.responsibleRange}</dd>
+                    <dt>Design Tool</dt>
+                    <dd>{work.design}</dd>
+                    {work.implement && (
+                      <>
+                        <dt>Implement</dt>
+                        <dd>{work.implement}</dd>
+                      </>
+                    )}
+                  </dl>
+                </div>
+              </li>
+              <li>
+                <div className={work.imageNameSc ? 'sp_img' : 'sp_img full_width'}>
+                  <img src={`${process.env.PUBLIC_URL}/images/${work.imageNamePc}`} alt={work.title} />
+                  {work.imageNameSc && (
+                    <img src={`${process.env.PUBLIC_URL}/images/${work.imageNameSc}`} alt={work.title} />
+                  )}
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="right_contnt">
-          <ul className="basic-text-inner">
-            <li>
-              <div className="catchCopy">{work.catchCopy}</div>
-              <p className="purpose">{work.summary}</p>
-              {work.detail.split('\n').map((line, index) => (
-                <React.Fragment key={index}>
-                  <p className="works_introduction">
-                  {line}
-                  </p>
-                </React.Fragment>
-              ))}
-              <div className="detail">
-                <dl>
-                  <dt>担当範囲</dt>
-                  <dd>{work.responsibleRange}</dd>
-                  <dt>Design Tool</dt>
-                  <dd>{work.design}</dd>
-                  {work.implement && (
-                    <>
-                      <dt>Implement</dt>
-                      <dd>{work.implement}</dd>
-                    </>
-                  )}
-                </dl>
-              </div>
-            </li>
-            <li>
-              <div className={work.imageNameSc ? 'sp_img' : 'sp_img full_width'}>
-                <img src={`${process.env.PUBLIC_URL}/images/${work.imageNamePc}`} alt={work.title} />
-                {work.imageNameSc && (
-                  <img src={`${process.env.PUBLIC_URL}/images/${work.imageNameSc}`} alt={work.title} />
-                )}
-              </div>
-            </li>
-          </ul>
-        </div>
       </div>
-    </div>
+    </FadeInSection>
   );
 };
