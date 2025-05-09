b60f264267189ede0ad863416beeb635eadcc5c3
mashita <axyxa0264@gmail.com>
2025-05-09 15:58:45 +0900

    portfolio_5.9: 各種アニメーションの追加

--- src/App.js
+++ src/App.js
@@ -6,28 +6,43 @@ import { WorkDetail } from './pages/Works/WorkDetail';
 import { AboutContent } from './pages/About/index';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ScrollToTop } from './components/ScrollToTop';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CONSTANTS } from './constants';
-
+import logoIcon from './images/header_logo.svg';
 
 function App() {
   const setIsOpen = useState(false)[1];
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <Router>
-      <ScrollToTop />
-      <Header
-        setIsOpen={setIsOpen}
-      />
-      <Routes>
-        <Route path={CONSTANTS.APP_NAME + '/'} element={<Home />} />
-        <Route path='/about/content' element={<AboutContent />} />
-        <Route path='/works/content'  element={<WorksContent />} />
-        <Route path='/workdetail/:id' element={<WorkDetail />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <>
+      {isLoading && (
+        <div className="loading_screen">
+          <div className="loading_logo">           
+            <img src={logoIcon} alt="logo" />
+          </div>
+          <h2 className="loading_title">portfolio site</h2>
+        </div>
+      )}
+      <Router>
+        <ScrollToTop />
+        <Header setIsOpen={setIsOpen} />
+        <Routes>
+          <Route path={CONSTANTS.APP_NAME + '/'} element={<Home />} />
+          <Route path='/about/content' element={<AboutContent />} />
+          <Route path='/works/content' element={<WorksContent />} />
+          <Route path='/workdetail/:id' element={<WorkDetail />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
-
 export default App;
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,49 +1,52 @@
 import React from 'react';
 import { AboutDigestSection } from './AboutDigestSection';
 import { aboutList } from '../../data/aboutList';
+import { FadeInSection } from '../../components/FadeInSection';
 
 export const AboutContent = () => {
   return (
-    <div className="about_section section">
-      <div className="about_header">
-        <h2 className="contact_title tracking-in-expand">about</h2>
-      </div>
+    <FadeInSection>
+      <div className="about_section section">
+        <div className="about_header">
+          <h2 className="contact_title tracking-in-expand">about</h2>
+        </div>
 
-      {aboutList.map((about, index) => (
-        <React.Fragment key={index}>
-          <AboutDigestSection
-            title={about.title}
-            subTitle={about.subTitle}
-            moreLink={about.link}
-          >
-              {about.summary}
-          </AboutDigestSection>
+        {aboutList.map((about, index) => (
+          <React.Fragment key={index}>
+            <AboutDigestSection
+              title={about.title}
+              subTitle={about.subTitle}
+              moreLink={about.link}
+            >
+                {about.summary}
+            </AboutDigestSection>
 
-          <div className="about_second_content">
-            <h3 className="about_title">biography</h3>
-            {about.biography?.map((bio, i) => (
-              <dl key={i} className="about_biography_list">
-                <dd>{bio.year}</dd>
-                <dt>{bio.summary}</dt>
-              </dl>
-            ))}
-          </div>
-          <div className="about_third_content">
-            <h3 className="about_title">skill</h3>
-            <ul className="about_skill_list">
-              {about.skill.map((skill, _index) => (
-              <li key={_index}>
-                <div className="about_skill_lists">
-                  <h4 className="about_skill_title">{skill.name}</h4>
-                  <p className="about_skill_category">{skill.category}</p>
-                </div>
-                  <p className="about_skill_summary">{skill.summary}</p>
-              </li>
+            <div className="about_second_content">
+              <h3 className="about_title">biography</h3>
+              {about.biography?.map((bio, i) => (
+                <dl key={i} className="about_biography_list">
+                  <dd>{bio.year}</dd>
+                  <dt>{bio.summary}</dt>
+                </dl>
               ))}
-            </ul>
-          </div>
-        </React.Fragment>
-      ))}
-    </div>
+            </div>
+            <div className="about_third_content">
+              <h3 className="about_title">skill</h3>
+              <ul className="about_skill_list">
+                {about.skill.map((skill, _index) => (
+                <li key={_index}>
+                  <div className="about_skill_lists">
+                    <h4 className="about_skill_title">{skill.name}</h4>
+                    <p className="about_skill_category">{skill.category}</p>
+                  </div>
+                    <p className="about_skill_summary">{skill.summary}</p>
+                </li>
+                ))}
+              </ul>
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
+    </FadeInSection>
   );
 };
